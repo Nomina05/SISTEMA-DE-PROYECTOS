71e85f4f2527299c0fd5fb8c1a6632f9afe80d4c
Nomina05 <mcoraamoca@gmail.com>
2026-05-07 21:43:54 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://aakvyrujjynvdwnsvnbt.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFha3Z5cnVqanludmR3bnN2bmJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNjIzNTYsImV4cCI6MjA5MzczODM1Nn0.4z_qfMFgIjrcE_Ha4oC3Z-xWyg8CnhPm4dHKkEvfo70";
 
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
