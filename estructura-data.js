5a85ef484b878d2729d7fa51105edadcc1a01c07
Nomina05 <mcoraamoca@gmail.com>
2026-05-25 10:01:40 -0400

    Add files via upload

---
+++ estructura-data.js
@@ -0,0 +1,148 @@
+window.CORAAMOCA_ORG_UNITS = `
+Tipo|Nombre|Depende de|Ruta
+Direccion|Direccion Administrativo y Financiero||Direccion Administrativo y Financiero
+Departamento|Departamento Administrativo|Direccion Administrativo y Financiero|Direccion Administrativo y Financiero > Departamento Administrativo
+Seccion|Activo Fijo|Departamento Administrativo|Direccion Administrativo y Financiero > Departamento Administrativo > Activo Fijo
+Seccion|Almacen y Suministro|Departamento Administrativo|Direccion Administrativo y Financiero > Departamento Administrativo > Almacen y Suministro
+Seccion|Compras y Contrataciones|Departamento Administrativo|Direccion Administrativo y Financiero > Departamento Administrativo > Compras y Contrataciones
+Division|Servicios Generales|Departamento Administrativo|Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales
+Seccion|Almacen y Suministro|Servicios Generales|Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Almacen y Suministro
+Seccion|Mantenimiento y Reparacion|Servicios Generales|Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Mantenimiento y Reparacion
+Seccion|Mayordomia|Servicios Generales|Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Mayordomia
+Seccion|Seguridad|Servicios Generales|Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Seguridad
+Seccion|Transportacion|Servicios Generales|Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Transportacion
+Departamento|Departamento Financiero|Direccion Administrativo y Financiero|Direccion Administrativo y Financiero > Departamento Financiero
+Seccion|Contabilidad|Departamento Financiero|Direccion Administrativo y Financiero > Departamento Financiero > Contabilidad
+Seccion|Presupuesto|Departamento Financiero|Direccion Administrativo y Financiero > Departamento Financiero > Presupuesto
+Seccion|Tesoreria|Departamento Financiero|Direccion Administrativo y Financiero > Departamento Financiero > Tesoreria
+Direccion|Direccion Comercial||Direccion Comercial
+Seccion|COM.|Direccion Comercial|Direccion Comercial > COM.
+Departamento|Gestion Comercial|Direccion Comercial|Direccion Comercial > Gestion Comercial
+Seccion|Atencion al Cliente|Gestion Comercial|Direccion Comercial > Gestion Comercial > Atencion al Cliente
+Seccion|Corte y Conexion|Gestion Comercial|Direccion Comercial > Gestion Comercial > Corte y Conexion
+Seccion|Facturacion|Gestion Comercial|Direccion Comercial > Gestion Comercial > Facturacion
+Seccion|Gestion de Cobros|Gestion Comercial|Direccion Comercial > Gestion Comercial > Gestion de Cobros
+Departamento|Gestion Operativa|Direccion Comercial|Direccion Comercial > Gestion Operativa
+Seccion|Catastro de Usuarios|Gestion Operativa|Direccion Comercial > Gestion Operativa > Catastro de Usuarios
+Seccion|Corte y Conexion|Gestion Operativa|Direccion Comercial > Gestion Operativa > Corte y Conexion
+Seccion|Gestion de Cobros|Gestion Operativa|Direccion Comercial > Gestion Operativa > Gestion de Cobros
+Seccion|Micro-Medicion|Gestion Operativa|Direccion Comercial > Gestion Operativa > Micro-Medicion
+Direccion|Direccion de Comunicaciones||Direccion de Comunicaciones
+Division|Division de Participacion Social y Ciudadana|Direccion de Comunicaciones|Direccion de Comunicaciones > Division de Participacion Social y Ciudadana
+Seccion|PROTOCOLO Y EVENTOS|Direccion de Comunicaciones|Direccion de Comunicaciones > PROTOCOLO Y EVENTOS
+Seccion|REDES SOCIALES Y MEDIOS DIGITALES|Direccion de Comunicaciones|Direccion de Comunicaciones > REDES SOCIALES Y MEDIOS DIGITALES
+Division|RELACIONES PUBLICAS|Direccion de Comunicaciones|Direccion de Comunicaciones > RELACIONES PUBLICAS
+Seccion|PRENSA|RELACIONES PUBLICAS|Direccion de Comunicaciones > RELACIONES PUBLICAS > PRENSA
+Direccion|Direccion de Planificacion y Desarrollo||Direccion de Planificacion y Desarrollo
+Departamento|Desarrollo Institucional Y Calidad En La Gestion|Direccion de Planificacion y Desarrollo|Direccion de Planificacion y Desarrollo > Desarrollo Institucional Y Calidad En La Gestion
+Departamento|Formulacion, Monitoreo Y Evaluacion De Planes, Programas Y Proyectos|Direccion de Planificacion y Desarrollo|Direccion de Planificacion y Desarrollo > Formulacion, Monitoreo Y Evaluacion De Planes, Programas Y Proyectos
+Direccion|Direccion de Recursos Humanos||Direccion de Recursos Humanos
+Seccion|Seccion de Evaluacion del Desempeno y Capacitaciones|Direccion de Recursos Humanos|Direccion de Recursos Humanos > Seccion de Evaluacion del Desempeno y Capacitaciones
+Seccion|Seccion de Organizacion del Trabajo y Compensaciones|Direccion de Recursos Humanos|Direccion de Recursos Humanos > Seccion de Organizacion del Trabajo y Compensaciones
+Seccion|Seccion de Registro, Control y Nomina|Direccion de Recursos Humanos|Direccion de Recursos Humanos > Seccion de Registro, Control y Nomina
+Direccion|Direccion General||Direccion General
+Departamento|Control y Analisis de Operaciones|Direccion General|Direccion General > Control y Analisis de Operaciones
+Departamento|Departamento Administrativo|Direccion General|Direccion General > Departamento Administrativo
+Division|Servicios Generales|Departamento Administrativo|Direccion General > Departamento Administrativo > Servicios Generales
+Seccion|Mayordomia|Servicios Generales|Direccion General > Departamento Administrativo > Servicios Generales > Mayordomia
+Seccion|Seguridad|Servicios Generales|Direccion General > Departamento Administrativo > Servicios Generales > Seguridad
+Departamento|Departamento de Control y Analisis de Operaciones|Direccion General|Direccion General > Departamento de Control y Analisis de Operaciones
+Departamento|Departamento De Tecnologia de la Informacion y Comunicacion (TIC)|Direccion General|Direccion General > Departamento De Tecnologia de la Informacion y Comunicacion (TIC)
+Seccion|Administracion de Servicios TIC|Departamento De Tecnologia de la Informacion y Comunicacion (TIC)|Direccion General > Departamento De Tecnologia de la Informacion y Comunicacion (TIC) > Administracion de Servicios TIC
+Seccion|Operaciones TIC|Departamento De Tecnologia de la Informacion y Comunicacion (TIC)|Direccion General > Departamento De Tecnologia de la Informacion y Comunicacion (TIC) > Operaciones TIC
+Seccion|Seccion de Operaciones TIC|Departamento De Tecnologia de la Informacion y Comunicacion (TIC)|Direccion General > Departamento De Tecnologia de la Informacion y Comunicacion (TIC) > Seccion de Operaciones TIC
+Departamento|Departamento Juridico|Direccion General|Direccion General > Departamento Juridico
+Direccion|Direccion Administrativo y Financiero|Direccion General|Direccion General > Direccion Administrativo y Financiero
+Departamento|Departamento Administrativo|Direccion Administrativo y Financiero|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo
+Seccion|Activo Fijo|Departamento Administrativo|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Activo Fijo
+Seccion|Compras y Contrataciones|Departamento Administrativo|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Compras y Contrataciones
+Division|Servicios Generales|Departamento Administrativo|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales
+Seccion|Almacen y Suministro|Servicios Generales|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Almacen y Suministro
+Seccion|Archivo y Correspondencia|Servicios Generales|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Archivo y Correspondencia
+Seccion|Mantenimiento y Reparacion|Servicios Generales|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Mantenimiento y Reparacion
+Seccion|Mayordomia|Servicios Generales|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Mayordomia
+Seccion|Seguridad|Servicios Generales|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Servicios Generales > Seguridad
+Seccion|Transportacion|Departamento Administrativo|Direccion General > Direccion Administrativo y Financiero > Departamento Administrativo > Transportacion
+Departamento|Departamento Financiero|Direccion Administrativo y Financiero|Direccion General > Direccion Administrativo y Financiero > Departamento Financiero
+Seccion|Contabilidad|Departamento Financiero|Direccion General > Direccion Administrativo y Financiero > Departamento Financiero > Contabilidad
+Seccion|Presupuesto|Departamento Financiero|Direccion General > Direccion Administrativo y Financiero > Departamento Financiero > Presupuesto
+Seccion|Tesoreria|Departamento Financiero|Direccion General > Direccion Administrativo y Financiero > Departamento Financiero > Tesoreria
+Direccion|Direccion Comercial|Direccion General|Direccion General > Direccion Comercial
+Departamento|Gestion Comercial|Direccion Comercial|Direccion General > Direccion Comercial > Gestion Comercial
+Seccion|Atencion al Cliente|Gestion Comercial|Direccion General > Direccion Comercial > Gestion Comercial > Atencion al Cliente
+Seccion|Centros de Pagos Externos|Gestion Comercial|Direccion General > Direccion Comercial > Gestion Comercial > Centros de Pagos Externos
+Seccion|Centros de Servicio al Cliente|Gestion Comercial|Direccion General > Direccion Comercial > Gestion Comercial > Centros de Servicio al Cliente
+Seccion|Facturacion|Gestion Comercial|Direccion General > Direccion Comercial > Gestion Comercial > Facturacion
+Seccion|Gestion de Cobros|Gestion Comercial|Direccion General > Direccion Comercial > Gestion Comercial > Gestion de Cobros
+Departamento|Gestion Operativa|Direccion Comercial|Direccion General > Direccion Comercial > Gestion Operativa
+Seccion|Catastro de Usuarios|Gestion Operativa|Direccion General > Direccion Comercial > Gestion Operativa > Catastro de Usuarios
+Seccion|Corte y Reconexion|Gestion Operativa|Direccion General > Direccion Comercial > Gestion Operativa > Corte y Reconexion
+Seccion|Micro-Medicion|Gestion Operativa|Direccion General > Direccion Comercial > Gestion Operativa > Micro-Medicion
+Direccion|Direccion de Comunicaciones|Direccion General|Direccion General > Direccion de Comunicaciones
+Division|Participacion Social y Ciudadana|Direccion de Comunicaciones|Direccion General > Direccion de Comunicaciones > Participacion Social y Ciudadana
+Seccion|Protocolo y Eventos|Direccion de Comunicaciones|Direccion General > Direccion de Comunicaciones > Protocolo y Eventos
+Seccion|Redes Sociales y Medios Digitales|Direccion de Comunicaciones|Direccion General > Direccion de Comunicaciones > Redes Sociales y Medios Digitales
+Division|Relaciones Publicas|Direccion de Comunicaciones|Direccion General > Direccion de Comunicaciones > Relaciones Publicas
+Seccion|Prensa|Relaciones Publicas|Direccion General > Direccion de Comunicaciones > Relaciones Publicas > Prensa
+Direccion|Direccion de Planificacion y Desarrollo|Direccion General|Direccion General > Direccion de Planificacion y Desarrollo
+Seccion|Cooperacion Internacional|Direccion de Planificacion y Desarrollo|Direccion General > Direccion de Planificacion y Desarrollo > Cooperacion Internacional
+Departamento|Desarrollo Institucional y Calidad en la Gestion|Direccion de Planificacion y Desarrollo|Direccion General > Direccion de Planificacion y Desarrollo > Desarrollo Institucional y Calidad en la Gestion
+Departamento|Formulacion, Monitoreo y Evaluacion de Planes, Programas y Proyectos|Direccion de Planificacion y Desarrollo|Direccion General > Direccion de Planificacion y Desarrollo > Formulacion, Monitoreo y Evaluacion de Planes, Programas y Proyectos
+Direccion|Direccion de Recursos Humanos|Direccion General|Direccion General > Direccion de Recursos Humanos
+Seccion|Evaluacion del Desempeno y Capacitaciones|Direccion de Recursos Humanos|Direccion General > Direccion de Recursos Humanos > Evaluacion del Desempeno y Capacitaciones
+Seccion|Organizacion del Trabajo y Compensaciones|Direccion de Recursos Humanos|Direccion General > Direccion de Recursos Humanos > Organizacion del Trabajo y Compensaciones
+Seccion|Registro, Control y Nomina|Direccion de Recursos Humanos|Direccion General > Direccion de Recursos Humanos > Registro, Control y Nomina
+Direccion|Direccion Tecnica|Direccion General|Direccion General > Direccion Tecnica
+Departamento|Aguas Residuales y Saneamiento|Direccion Tecnica|Direccion General > Direccion Tecnica > Aguas Residuales y Saneamiento
+Seccion|Control de Calidad de Aguas Residuales|Aguas Residuales y Saneamiento|Direccion General > Direccion Tecnica > Aguas Residuales y Saneamiento > Control de Calidad de Aguas Residuales
+Seccion|Operacion y Mantenimiento de Redes de Aguas Residuales|Aguas Residuales y Saneamiento|Direccion General > Direccion Tecnica > Aguas Residuales y Saneamiento > Operacion y Mantenimiento de Redes de Aguas Residuales
+Seccion|Tratamiento de Aguas Residuales|Aguas Residuales y Saneamiento|Direccion General > Direccion Tecnica > Aguas Residuales y Saneamiento > Tratamiento de Aguas Residuales
+Departamento|Ingenieria|Direccion Tecnica|Direccion General > Direccion Tecnica > Ingenieria
+Seccion|Construccion|Ingenieria|Direccion General > Direccion Tecnica > Ingenieria > Construccion
+Seccion|Diseno y Presupuesto|Ingenieria|Direccion General > Direccion Tecnica > Ingenieria > Diseno y Presupuesto
+Seccion|Fiscalizacion y Control de Obras|Ingenieria|Direccion General > Direccion Tecnica > Ingenieria > Fiscalizacion y Control de Obras
+Departamento|Operacion y Mantenimiento|Direccion Tecnica|Direccion General > Direccion Tecnica > Operacion y Mantenimiento
+Seccion|Catastro de Redes de Agua Potable|Operacion y Mantenimiento|Direccion General > Direccion Tecnica > Operacion y Mantenimiento > Catastro de Redes de Agua Potable
+Seccion|Electromecanica|Operacion y Mantenimiento|Direccion General > Direccion Tecnica > Operacion y Mantenimiento > Electromecanica
+Seccion|Mantenimiento de Redes|Operacion y Mantenimiento|Direccion General > Direccion Tecnica > Operacion y Mantenimiento > Mantenimiento de Redes
+Seccion|Operacion y Distribucion de Agua Potable|Operacion y Mantenimiento|Direccion General > Direccion Tecnica > Operacion y Mantenimiento > Operacion y Distribucion de Agua Potable
+Seccion|Operacion y Mantenimiento de Redes de Zonas Perifericas|Operacion y Mantenimiento|Direccion General > Direccion Tecnica > Operacion y Mantenimiento > Operacion y Mantenimiento de Redes de Zonas Perifericas
+Departamento|Produccion y Tratamiento de Agua Potable|Direccion Tecnica|Direccion General > Direccion Tecnica > Produccion y Tratamiento de Agua Potable
+Seccion|Control y Calidad de Agua Potable|Produccion y Tratamiento de Agua Potable|Direccion General > Direccion Tecnica > Produccion y Tratamiento de Agua Potable > Control y Calidad de Agua Potable
+Seccion|Laboratorio de Agua Potable|Produccion y Tratamiento de Agua Potable|Direccion General > Direccion Tecnica > Produccion y Tratamiento de Agua Potable > Laboratorio de Agua Potable
+Seccion|Obra de Toma|Produccion y Tratamiento de Agua Potable|Direccion General > Direccion Tecnica > Produccion y Tratamiento de Agua Potable > Obra de Toma
+Seccion|Tratamiento de Agua Potable|Produccion y Tratamiento de Agua Potable|Direccion General > Direccion Tecnica > Produccion y Tratamiento de Agua Potable > Tratamiento de Agua Potable
+Departamento|Juridico|Direccion General|Direccion General > Juridico
+Departamento|Oficina de Acceso a la Informacion|Direccion General|Direccion General > Oficina de Acceso a la Informacion
+Seccion|Oficina de Acceso a la Informacion Publica|Direccion General|Direccion General > Oficina de Acceso a la Informacion Publica
+Departamento|Operacion y Mantenimiento|Direccion General|Direccion General > Operacion y Mantenimiento
+Seccion|Operacion y Distribucion de Agua Potable|Operacion y Mantenimiento|Direccion General > Operacion y Mantenimiento > Operacion y Distribucion de Agua Potable
+Division|Participacion Social y Ciudadana|Direccion General|Direccion General > Participacion Social y Ciudadana
+Departamento|Produccion y Tratamiento de Agua Potable|Direccion General|Direccion General > Produccion y Tratamiento de Agua Potable
+Seccion|Control y Calidad de Agua Potable|Produccion y Tratamiento de Agua Potable|Direccion General > Produccion y Tratamiento de Agua Potable > Control y Calidad de Agua Potable
+Seccion|Laboratorio de Agua Potable|Produccion y Tratamiento de Agua Potable|Direccion General > Produccion y Tratamiento de Agua Potable > Laboratorio de Agua Potable
+Seccion|Tratamiento de Agua Potable|Produccion y Tratamiento de Agua Potable|Direccion General > Produccion y Tratamiento de Agua Potable > Tratamiento de Agua Potable
+Division|Relaciones Publicas|Direccion General|Direccion General > Relaciones Publicas
+Direccion|Direccion Tecnica||Direccion Tecnica
+Departamento|Aguas Residuales y Saneamiento|Direccion Tecnica|Direccion Tecnica > Aguas Residuales y Saneamiento
+Seccion|Control de Calidad de Aguas Residuales|Aguas Residuales y Saneamiento|Direccion Tecnica > Aguas Residuales y Saneamiento > Control de Calidad de Aguas Residuales
+Seccion|Operacion y Mantenimiento de Redes de Aguas Residuales|Aguas Residuales y Saneamiento|Direccion Tecnica > Aguas Residuales y Saneamiento > Operacion y Mantenimiento de Redes de Aguas Residuales
+Seccion|Tratamiento de Aguas Residuales|Aguas Residuales y Saneamiento|Direccion Tecnica > Aguas Residuales y Saneamiento > Tratamiento de Aguas Residuales
+Departamento|Ingenieria|Direccion Tecnica|Direccion Tecnica > Ingenieria
+Seccion|Construccion|Ingenieria|Direccion Tecnica > Ingenieria > Construccion
+Seccion|Diseno y Presupuesto|Ingenieria|Direccion Tecnica > Ingenieria > Diseno y Presupuesto
+Seccion|Fiscalizacion y Control de Obras|Ingenieria|Direccion Tecnica > Ingenieria > Fiscalizacion y Control de Obras
+Departamento|Operacion y Mantenimiento|Direccion Tecnica|Direccion Tecnica > Operacion y Mantenimiento
+Seccion|Electromecanica|Operacion y Mantenimiento|Direccion Tecnica > Operacion y Mantenimiento > Electromecanica
+Seccion|Mantenimiento de Redes|Operacion y Mantenimiento|Direccion Tecnica > Operacion y Mantenimiento > Mantenimiento de Redes
+Seccion|Operacion y Distribucion de Agua Potable|Operacion y Mantenimiento|Direccion Tecnica > Operacion y Mantenimiento > Operacion y Distribucion de Agua Potable
+Seccion|Operacion y Mantenimiento de Redes|Operacion y Mantenimiento|Direccion Tecnica > Operacion y Mantenimiento > Operacion y Mantenimiento de Redes
+Seccion|Operacion y Mantenimiento de Zonas Perifericas|Operacion y Mantenimiento|Direccion Tecnica > Operacion y Mantenimiento > Operacion y Mantenimiento de Zonas Perifericas
+Departamento|Producion y Tratamiento de Agua Potable|Direccion Tecnica|Direccion Tecnica > Producion y Tratamiento de Agua Potable
+Seccion|Control de Calidad de Agua Potable|Producion y Tratamiento de Agua Potable|Direccion Tecnica > Producion y Tratamiento de Agua Potable > Control de Calidad de Agua Potable
+Seccion|Laboratorio de Agua Potable|Producion y Tratamiento de Agua Potable|Direccion Tecnica > Producion y Tratamiento de Agua Potable > Laboratorio de Agua Potable
+Seccion|Tratamiento de Agua Potable|Producion y Tratamiento de Agua Potable|Direccion Tecnica > Producion y Tratamiento de Agua Potable > Tratamiento de Agua Potable
+`.trim().split("\n").slice(1).map(line => {
+  const [tipo, nombre, padre, ruta] = line.split("|");
+  return { tipo, nombre, padre, ruta };
+});
